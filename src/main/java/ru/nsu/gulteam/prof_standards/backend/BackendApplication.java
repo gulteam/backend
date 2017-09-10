a2340be75586b115f38cf8ac9e1037191af93085
batalin <batalin@azoft.com>
2017-09-10 19:54:27 +0700

    Add mapstruct

--- src/main/java/ru/nsu/gulteam/prof_standards/backend/BackendApplication.java
+++ src/main/java/ru/nsu/gulteam/prof_standards/backend/BackendApplication.java
@@ -2,6 +2,8 @@ package ru.nsu.gulteam.prof_standards.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
@@ -9,9 +11,14 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableNeo4jRepositories
-public class BackendApplication {
+public class BackendApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BackendApplication.class);
 	}
 }
