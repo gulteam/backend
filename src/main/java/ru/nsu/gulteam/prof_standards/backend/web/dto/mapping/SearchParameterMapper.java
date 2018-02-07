11e62f6da639efdd3d52cce52bdf14536d571e27
Kirill Batalin <kir55rus@gmail.com>
2018-02-07 16:31:34 +0700

    feature(mappers) Refactoring mappers

--- src/main/java/ru/nsu/gulteam/prof_standards/backend/web/dto/mapping/SearchParameterMapper.java
+++ src/main/java/ru/nsu/gulteam/prof_standards/backend/web/dto/mapping/SearchParameterMapper.java
@@ -1,41 +1,13 @@
 package ru.nsu.gulteam.prof_standards.backend.web.dto.mapping;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import ru.nsu.gulteam.prof_standards.backend.domain.node.ProfessionalStandard;
+import org.mapstruct.Mapper;
 import ru.nsu.gulteam.prof_standards.backend.entity.FullSearchRequest;
 import ru.nsu.gulteam.prof_standards.backend.web.dto.request.SearchParameters;
 
-import java.util.stream.Collectors;
-
-@Configuration
-public class SearchParameterMapper {
-    private CourseMapper courseMapper;
-    private ProgramMapper programMapper;
-    private ProfessionalStandardMapper professionalStandardMapper;
-
-    @Autowired
-    public SearchParameterMapper(CourseMapper courseMapper, ProgramMapper programMapper, ProfessionalStandardMapper professionalStandardMapper) {
-        this.courseMapper = courseMapper;
-        this.programMapper = programMapper;
-        this.professionalStandardMapper = professionalStandardMapper;
-    }
-
-    public SearchParameterMapper() {
-    }
-
-    @Bean
-    public SearchParameterMapper createSearchParameterMapper(CourseMapper courseMapper, ProgramMapper programMapper, ProfessionalStandardMapper professionalStandardMapper){
-        return new SearchParameterMapper(courseMapper, programMapper, professionalStandardMapper);
-    }
-
-    public FullSearchRequest fromDto(SearchParameters searchParameters){
-        return new FullSearchRequest(programMapper.fromDto(searchParameters.getBasicEducationProgram()),
-                searchParameters.getIncludeCourses().stream().map(courseMapper::fromDto).collect(Collectors.toList()),
-                searchParameters.getExcludeCourses().stream().map(courseMapper::fromDto).collect(Collectors.toList()),
-                searchParameters.getIncludeStandards().stream().map(professionalStandardMapper::fromDto).collect(Collectors.toList()),
-                searchParameters.getExcludeStandards().stream().map(professionalStandardMapper::fromDto).collect(Collectors.toList())
-                );
-    }
+@Mapper(componentModel = "spring",
+        uses = {CourseMapper.class,
+                ProgramMapper.class,
+                ProfessionalStandardMapper.class})
+public interface SearchParameterMapper {
+    FullSearchRequest fromDto(SearchParameters searchParameters);
 }
--- src/main/java/ru/nsu/gulteam/prof_standards/backend/web/dto/mapping/UserMapper.java
+++ src/main/java/ru/nsu/gulteam/prof_standards/backend/web/dto/mapping/UserMapper.java
@@ -1,77 +1,44 @@
 package ru.nsu.gulteam.prof_standards.backend.web.dto.mapping;
 
+import org.mapstruct.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import ru.nsu.gulteam.prof_standards.backend.domain.node.User;
 import ru.nsu.gulteam.prof_standards.backend.entity.FullUserInfo;
 import ru.nsu.gulteam.prof_standards.backend.web.dto.request.RegisterData;
 import ru.nsu.gulteam.prof_standards.backend.web.dto.response.UserDto;
 
-@Configuration
-public class UserMapper {
-    private PasswordEncoder passwordEncoder;
-    private RoleMapper roleMapper;
-    private FacultyMapper facultyMapper;
-    private DepartmentMapper departmentMapper;
-
+@Mapper(componentModel = "spring",
+        uses = {RoleMapper.class,
+                FacultyMapper.class,
+                DepartmentMapper.class})
+public abstract class UserMapper {
     @Autowired
-    public UserMapper(PasswordEncoder passwordEncoder, RoleMapper roleMapper, FacultyMapper facultyMapper, DepartmentMapper departmentMapper) {
-        this.passwordEncoder = passwordEncoder;
-        this.roleMapper = roleMapper;
-        this.facultyMapper = facultyMapper;
-        this.departmentMapper = departmentMapper;
-    }
+    private PasswordEncoder passwordEncoder;
 
-    @Bean
-    public UserMapper createUserMapper(PasswordEncoder passwordEncoder, RoleMapper roleMapper, FacultyMapper facultyMapper, DepartmentMapper departmentMapper) {
-        return new UserMapper(passwordEncoder, roleMapper, facultyMapper, departmentMapper);
-    }
+    @Mappings({
+            @Mapping(target = "id", source = "user.id"),
+            @Mapping(target = "firstName", source = "user.firstName"),
+            @Mapping(target = "secondName", source = "user.secondName"),
+            @Mapping(target = "login", source = "user.login"),
+    })
+    public abstract UserDto toDto(FullUserInfo user);
 
-    public UserDto toDto(FullUserInfo user){
-        UserDto userDto = new UserDto();
+    @Mappings({
+            @Mapping(target = "user.firstName", source = "firstName"),
+            @Mapping(target = "user.secondName", source = "secondName"),
+            @Mapping(target = "user.login", source = "login"),
+    })
+    public abstract FullUserInfo fromDto(UserDto userDto);
 
-        User userEntity = user.getUser();
+    @Mappings({
+            @Mapping(target = "login", source = "authData.login"),
+            @Mapping(target = "passwordHash", ignore = true),
+    })
+    public abstract User toUser(RegisterData registerData);
 
-        userDto.setFirstName(userEntity.getFirstName());
-        userDto.setSecondName(userEntity.getSecondName());
-        userDto.setLogin(userEntity.getLogin());
-        userDto.setId(userEntity.getId());
-
-        userDto.setRole(roleMapper.toDto(user.getRole()));
-        userDto.setDepartment(departmentMapper.toDto(user.getDepartment()));
-        userDto.setFaculty(facultyMapper.toDto(user.getFaculty()));
-
-        return userDto;
-    };
-
-
-    public FullUserInfo fromDto(UserDto userDto){
-        FullUserInfo fullUserInfo = new FullUserInfo();
-
-        fullUserInfo.setUser(new User(userDto.getId(), userDto.getFirstName(), userDto.getSecondName(), userDto.getLogin()));
-        fullUserInfo.setRole(roleMapper.fromDto(userDto.getRole()));
-
-        if(userDto.getFaculty() != null){
-            fullUserInfo.setFaculty(facultyMapper.fromDto(userDto.getFaculty()));
-        }
-
-        if(userDto.getDepartment() != null){
-            fullUserInfo.setDepartment(departmentMapper.fromDto(userDto.getDepartment()));
-        }
-
-        return fullUserInfo;
-    }
-
-    public User toUser(RegisterData registerData){
-        User user = new User();
-
-        user.setFirstName(registerData.getFirstName());
-        user.setSecondName(registerData.getSecondName());
-        user.setLogin(registerData.getAuthData().getLogin());
+    @AfterMapping
+    protected void afterMapping(RegisterData registerData, @MappingTarget User user) {
         user.setPasswordHash(passwordEncoder.encode(registerData.getAuthData().getPassword()));
-
-        return user;
     }
 }
